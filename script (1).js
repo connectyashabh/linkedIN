fb253cc4951b1d412f655aeee5b14064990a258e
connectyashabh <53211928+connectyashabh@users.noreply.github.com>
2021-03-12 23:47:34 +0530

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,25 @@
+document.getElementById("visit").onclick = function () {
+    location.href = "https://campk12.com";
+};
+
+document.getElementById("play").onclick = function () {
+    location.href = "https://www.youtube.com/watch?v=huE8xnsXs4w";
+};
+document.getElementById("skillshare").onclick = function () {
+    location.href = "https://www.skillshare.com";
+};
+document.getElementById("edX").onclick = function () {
+    location.href = "https://www.edx.org";
+};
+document.getElementById("donors").onclick = function () {
+    location.href = "https://www.donorschoose.org/";
+};
+document.getElementById("job").onclick = function () {
+    location.href = "https://angel.co/company/campk12/jobs";
+};
+document.getElementById("linkedIn").onclick = function () {
+    location.href = "https://in.linkedin.com";
+};
+document.getElementById("linked").onclick = function () {
+    location.href = "https://in.linkedin.com";
+};
